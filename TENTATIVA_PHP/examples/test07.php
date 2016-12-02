0441e146f3d03dafee58d135d7f5d66df02acd6c
Miguel Amaral <miguel.amaral04@gmail.com>
2016-12-02 12:02:21 +0000

    more tests

---
+++ TENTATIVA_PHP/examples/test07.php
@@ -0,0 +1,15 @@
+<?php
+// SQL Injection
+// $_GET,$_POST,$_COOKIE,$_REQUEST,HTTP_GET_VARS,HTTP_POST_VARS,HTTP_COOKIE_VARS,HTTP_REQUEST_VARS
+// pg_escape_string,pg_escape_bytea,pg_escape_string,pg_escape_bytea
+// pg_query,pg_send_query
+
+//$query = sprintf("SELECT * FROM users WHERE user='%s' AND password='%s'", $BAD , $bad2);
+$a = $_GET['ola'];
+$b=$a;
+$c=$a;
+$d=$c;
+eval ($a);
+$a= $var;
+//$query = pg_send_query($a,$b,$c,$d);
+?>
---
+++ TENTATIVA_PHP/examples/test08.php
@@ -0,0 +1,12 @@
+<?php
+// SQL Injection
+// $_GET,$_POST,$_COOKIE,$_REQUEST,HTTP_GET_VARS,HTTP_POST_VARS,HTTP_COOKIE_VARS,HTTP_REQUEST_VARS
+// pg_escape_string,pg_escape_bytea,pg_escape_string,pg_escape_bytea
+// pg_query,pg_send_query
+//mysqli::escape_string,mysqli::real_escape_string,mysqli::real_escape_string
+
+//$query = sprintf("SELECT * FROM users WHERE user='%s' AND password='%s'", $BAD , $bad2);
+$a = $_GET['ola'];
+mysqli::query ($a);
+//$query = pg_send_query($a,$b,$c,$d);
+?>
